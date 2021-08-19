aa2f5a228a5c09f58ea320bf8547f1d3284b0c97
babakir73@gmail.com <babakir73@gmail.com>
2021-08-19 22:56:11 +0430

    script loader :) thanks God

--- editor/polyfills.js
+++ editor/polyfills.js
@@ -1,25 +1,25 @@
 
-import '../examples/js/libs/draco/draco_encoder.js';
+import 'script-loader!../examples/js/libs/draco/draco_encoder.js';
 
-import './js/libs/codemirror/codemirror.js';
-import './js/libs/codemirror/mode/javascript.js';
-import './js/libs/codemirror/mode/glsl.js';
+import 'script-loader!./js/libs/codemirror/codemirror.js';
+import 'script-loader!./js/libs/codemirror/mode/javascript.js';
+import 'script-loader!./js/libs/codemirror/mode/glsl.js';
 
-import './js/libs/esprima.js';
-import './js/libs/jsonlint.js';
+import 'script-loader!./js/libs/esprima.js';
+import 'script-loader!./js/libs/jsonlint.js';
 
-import './js/libs/codemirror/addon/dialog.js';
-import './js/libs/codemirror/addon/show-hint.js';
-import './js/libs/codemirror/addon/tern.js';
-import './js/libs/acorn/acorn.js';
-import './js/libs/acorn/acorn_loose.js';
-import './js/libs/acorn/walk.js';
-import './js/libs/ternjs/polyfill.js';
-import './js/libs/ternjs/signal.js';
-import './js/libs/ternjs/tern.js';
-import './js/libs/ternjs/def.js';
-import './js/libs/ternjs/comment.js';
-import './js/libs/ternjs/infer.js';
-import './js/libs/ternjs/doc_comment.js';
-import './js/libs/tern-threejs/threejs.js';
-import './js/libs/signals.min.js';
+import 'script-loader!./js/libs/codemirror/addon/dialog.js';
+import 'script-loader!./js/libs/codemirror/addon/show-hint.js';
+import 'script-loader!./js/libs/codemirror/addon/tern.js';
+import 'script-loader!./js/libs/acorn/acorn.js';
+import 'script-loader!./js/libs/acorn/acorn_loose.js';
+import 'script-loader!./js/libs/acorn/walk.js';
+import 'script-loader!./js/libs/ternjs/polyfill.js';
+import 'script-loader!./js/libs/ternjs/signal.js';
+import 'script-loader!./js/libs/ternjs/tern.js';
+import 'script-loader!./js/libs/ternjs/def.js';
+import 'script-loader!./js/libs/ternjs/comment.js';
+import 'script-loader!./js/libs/ternjs/infer.js';
+import 'script-loader!./js/libs/ternjs/doc_comment.js';
+import 'script-loader!./js/libs/tern-threejs/threejs.js';
+import 'script-loader!./js/libs/signals.min.js';
